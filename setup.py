12ba03c6142c8122092d06b670ad3c4f76aa6ff4
Tmpod <35771816+Tmpod@users.noreply.github.com>
2020-05-16 11:55:49 +0000

    Remove seemingly unneeded psycopg dep for asyncpg

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup(
     data_files=[("", ["LICENSE.md"])],
     install_requires=["sqlalchemy", 'aiocontextvars;python_version<"3.7"'],
     extras_require={
-        "postgresql": ["asyncpg", "psycopg2"],
+        "postgresql": ["asyncpg"],
         "mysql": ["aiomysql", "pymysql"],
         "sqlite": ["aiosqlite"],
         "postgresql+aiopg": ["aiopg"]
